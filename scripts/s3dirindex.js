786c60030b622621fae0905f3442a8f031d2e368
Evan Rosson <git@erosson.org>
2021-02-22 13:55:35 -0500

    fix diff; quieter syncs

--- scripts/s3dirindex.js
+++ scripts/s3dirindex.js
@@ -58,7 +58,8 @@ async function runSync(src, dest, args=[]) {
   return new Promise((resolve, reject) => {
     console.log(`sync: ${src} --> ${dest}`)
     const p = child_process.spawn("aws", ["s3", "sync", src, dest].concat(args))
-    p.stdout.on('data', data => console.log(`${data}`))
+    // p.stdout.on('data', data => console.log(`${data}`))
+    p.stdout.on('data', data => null)
     p.stderr.on('data', data => console.error(`${data}`))
     p.on('close', code => {
       if (code) {
